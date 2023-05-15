810d804ec0418a527464027c835320aff003688d
Deniz Cakmak <d.cakmak@emakina.com.tr>
2023-05-15 18:56:38 +0300

    Create FileSystemVisitor class to traverse a tree of folders on the file system.

---
+++ AdvCSharp/FileSystemVisitor.cs
@@ -0,0 +1,46 @@
+﻿namespace AdvCSharp
+{
+    internal class FileSystemVisitor
+    {
+        private readonly string rootFolder;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileSystemVisitor"/> class.
+        /// </summary>
+        /// <param name="rootFolder">Root folder path.</param>
+        public FileSystemVisitor(string rootFolder)
+        {
+            this.rootFolder = rootFolder ?? throw new ArgumentNullException(nameof(rootFolder));
+        }
+
+        /// <summary>
+        /// Traverse through folders and files of root folder.
+        /// </summary>
+        /// <returns>IEnumerable string of folders and files in linear sequence.</returns>
+        public IEnumerable<string> Traverse()
+        {
+            return this.Traverse(this.rootFolder);
+        }
+
+        private IEnumerable<string> Traverse(string path)
+        {
+            // Return current folder path
+            yield return path;
+
+            // Iterate and return each file in the current folder path
+            foreach (var file in Directory.GetFiles(path))
+            {
+                yield return file;
+            }
+
+            // Iterates each folder in the current folder path, recursive call foreach subfolder
+            foreach (var subFolder in Directory.GetDirectories(path))
+            {
+                foreach (var item in this.Traverse(subFolder))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
--- AdvCSharp/Program.cs
+++ AdvCSharp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdvCSharp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("To close the app type 'exit'.");
+        string? text = null;
+        do
+        {
+            Console.WriteLine("Enter root folder path: ");
+            text = Console.ReadLine();
+
+            if (text == "exit")
+            {
+                Environment.Exit(0);
+            }
+            else if (string.IsNullOrEmpty(text) || !text.StartsWith("C:"))
+            {
+                Console.WriteLine("Please enter valid root path.");
+                continue;
+            }
+
+            // Work
+            FileSystemVisitor fileSystemVisitor = new FileSystemVisitor(text);
+            var content = fileSystemVisitor.Traverse();
+
+            foreach (var c in content)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        while (string.IsNullOrWhiteSpace(text) || !text.StartsWith("C:"));
+    }
+}
